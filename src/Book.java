9ce056f80ec4b2c3057880036d13fa8ff11eb162
georg2990 <georg2990@gmail.com>
2025-09-17 09:51:08 +0200

    Georg

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,21 @@
-public class Book {
+public class Book extends loanItem {
+    public Book(String name) {
+        super(name);
+
+    }
+
+    @Override
+    public String getType() {
+        return "Book";
+    }
+
+    @Override
+    public String getName() {
+        return "Title";
+    }
+
+    @Override
+    public String toString(){
+        return "Book" + "Title";
+    }
 }
