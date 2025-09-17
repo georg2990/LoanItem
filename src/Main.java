1b68a7bce0f32b806880c6797cc93b48ec302678
Adam <adzi1000@stud.ek.dk>
2025-09-17 10:18:52 +0200

    adam ziada

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,34 @@ public class Main {
         int numberOfItems = scanner.nextInt();
         scanner.nextLine();
 
+        // til at gemme input,,,,,
+        String[] types = new String[numberOfItems];
+        String[] titles = new String[numberOfItems];
+
         //spøger om tittel og type
         for (int i = 0; i < numberOfItems; i++) {
-            System.out.print("Titre: ");
-            String title = scanner.nextLine();
+            System.out.print("Bog eller video?: ");
+            String Type = scanner.nextLine();
+
+            System.out.print("Hvad er titlen: ");
+            String Title = scanner.nextLine();
+            }
+        //udskriv tabel
+        System.out.println("\nListe af lånde items");
+        System.out.println("----------------------");
+        System.out.println("ID, TYPE, TITLE");
+
+        for (int i = 0; i < numberOfItems; i++) {
+
+
+        }
+
+
+
+
+
+
+
 
 
 
@@ -34,4 +58,3 @@ public class Main {
 
 
     }
-}
