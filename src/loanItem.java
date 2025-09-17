59eae02f9d2839102cf7a1e61c98ea38579503de
Adam <adzi1000@stud.ek.dk>
2025-09-17 09:21:11 +0200

    adam ziada

--- src/loanItem.java
+++ src/loanItem.java
@@ -1,2 +1,20 @@
+//super class
 public class loanItem {
+    String title;
+    String type;
+
+    // Constructor
+    public loanItem(String navn, String type) {
+        this.title = title;
+        this.type = type;
+    }
+
+    public String getName() {
+        return title;
+    }
+    public String getType() {
+        return type;
+    }
 }
+
+
