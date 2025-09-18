49838bee9316870372f9435ba2f4f01e81359d26
georg2990 <georg2990@gmail.com>
2025-09-18 14:20:15 +0200

    committed

--- src/loanItemTest.java
+++ src/loanItemTest.java
@@ -3,22 +3,42 @@ public class loanItemTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Velkomst besked
         System.out.println("Velkommen til Loan Items!");
-        System.out.println("Hvor mange items kunne du tænke dig at registrere? ");
-        int numberOfItems = scanner.nextInt();
+
+        int numberOfItems = 0;
+
+        // while-loop til at sikre positivt heltal
+        while (numberOfItems <= 0) {
+            System.out.print("Hvor mange items vil du registrere? ");
+
+            // tjekker om input er et heltal
+            if (scanner.hasNextInt()) {
+                numberOfItems = scanner.nextInt();
+
+                // hvis tallet er 0 eller negativt
+                if (numberOfItems <= 0) {
+                    System.out.println("indtast venligst et positivt tal.");
+                }
+            } else {
+                // hvis input ikke er et tal
+                System.out.println("det skal være et positivt tal.");
+                scanner.next();
+            }
+        }
         scanner.nextLine();
 
-        // til at gemme input
+        // array til at gemme input
         loanItem[] loanItems = new loanItem[numberOfItems];
 
         //spøger om tittel og type
         for (int i = 0; i < numberOfItems; i++) {
             System.out.println("Registrer Item nr. " + (i + 1));
             System.out.print("Type: bog/video?: ");
-            String Type = scanner.next().toLowerCase();
+            String Type = scanner.nextLine().toLowerCase();
 
             System.out.print("Title: ");
-            String Title = scanner.next();
+            String Title = scanner.nextLine();
 
             if (Type.equals("bog")) {
                 loanItems[i] = new Book(i + 1, Title, "Bog");
@@ -32,6 +52,7 @@ public class loanItemTest {
         System.out.println("ID, TYPE, TITLE");
 
         for (int i = 0; i < loanItems.length; i++) {
-                System.out.println((i + 1) + "\t" + loanItems[i].title + "   " + loanItems[i].type + "  ".toString());
+                System.out.println((i + 1) + "\t" + loanItems[i]);
             }
-        }}
+    }
+}
